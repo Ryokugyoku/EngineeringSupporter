b192120577356e47b9eb36e6127ff011b2d87581
ryokugyoku <ryoku@blueremarks.com>
2026-01-27 00:00:31 +0900

    `GitHub Actionsリリース設定を更新し、mainブランチのプッシュ時にプレビューバージョンを生成可能に修正`

--- EngineeringSupporter.WinUI/Program.cs
+++ EngineeringSupporter.WinUI/Program.cs
@@ -14,7 +14,7 @@ public static class Program
         VelopackApp.Build().Run();
 
         // 2. 通常のWinUI起動プロセス
-        Application.Start((p) =>
+        Microsoft.UI.Xaml.Application.Start((p) =>
         {
             var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
             SynchronizationContext.SetSynchronizationContext(context);
